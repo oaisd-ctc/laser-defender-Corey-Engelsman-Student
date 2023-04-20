27789f0a1c0ac02898a83ad2ddd2cb39e72ca06f
Corey-Engelsman-Student <112406534+Corey-Engelsman-Student@users.noreply.github.com>
2023-04-20 11:03:15 -0400

    initial commit of Laser Defender

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxbounds;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+    void start()
+    {
+        InitBounds();
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxbounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+    }
+
+    void Move()
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxbounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxbounds.y - paddingTop);
+        transform.position = newPos;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+}
